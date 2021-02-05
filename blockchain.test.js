b4a10ad43e5165f08e504b2be4a532cc09137737
Kshitij Malvankar <kshitijmalvankar@gmail.com>
2021-02-05 13:49:32 +0000

    Update: Added tests for chain validation

--- blockchain.test.js
+++ blockchain.test.js
@@ -3,7 +3,11 @@ const Block = require('./block');
 
 
 describe('Blockchain',()=>{
-    const blockchain = new Blockchain();
+    let blockchain;
+
+    beforeEach(()=>{
+        blockchain = new Blockchain();
+    })
 
     it('contains a `chain` Array instance',()=>{
         expect(blockchain.chain instanceof Array).toBe(true);
@@ -18,5 +22,49 @@ describe('Blockchain',()=>{
         blockchain.addBlock({data: newData});
 
         expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
+    });
+
+    describe('isValidChain()',()=>{
+        describe('chain doest start with a genesis block',()=>{
+            it('returns false',()=>{
+                blockchain.chain[0] = {data: 'fake-genesis'};
+
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+            });
+        });
+
+        describe('chain starts with genesis block and has multiple blocks',()=>{
+
+            beforeEach(()=>{
+                blockchain.addBlock({ data: 'Data'});
+                blockchain.addBlock({ data: 'Data 2'});
+                blockchain.addBlock({ data: 'Data 3'});
+
+            });
+
+            describe('last hash reference has changed',()=>{
+                it('returns false',()=>{
+                   
+                    blockchain.chain[2].lastHash = 'fake lastHash';
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                });
+
+            });
+
+            describe('and chain contains a block with an invalid field',()=>{
+                it('returns false',()=>{
+                  
+                    blockchain.chain[2].data = 'bad-data';
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                });
+            });
+
+            describe('chain doesnt contain any invalid blocks',()=>{
+                it('returns true',()=>{
+
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+                });
+            });
+        });
     });
 });
