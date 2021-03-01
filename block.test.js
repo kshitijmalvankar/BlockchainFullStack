32fb823d7118410e1c3f2f204ec23e9fa035da73
Kshitij Malvankar <kshitijmalvankar@gmail.com>
2021-03-01 16:54:56 +0000

    Update: Added test cases for nonce and difficulty

--- block.test.js
+++ block.test.js
@@ -7,8 +7,10 @@ describe('Block', ()=> {
     const lastHash = 'hash';
     const hash = 'new hash';
     const data = ['blockchain', 'data'];
+    const nonce = 1;
+    const difficulty = 1;
     const block = new Block({
-        timestamp, lastHash, hash, data
+        timestamp, lastHash, hash, data, nonce, difficulty
     });
 
     it('has a timestamp, lastHash, hash and data properties',()=>{
@@ -16,6 +18,9 @@ describe('Block', ()=> {
         expect(block.lastHash).toEqual(lastHash);
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data);
+        expect(block.nonce).toEqual(nonce);
+        expect(block.difficulty).toEqual(difficulty);
+
     });
 
 
@@ -55,7 +60,16 @@ describe('Block', ()=> {
         });
 
         it('creates a SHA-256 `hash` based on the proper inputs',()=>{
-            expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
+            expect(minedBlock.hash).toEqual(cryptoHash(
+                minedBlock.timestamp,
+                minedBlock.nonce,
+                minedBlock.difficulty, 
+                lastBlock.hash, 
+                data));
+        });
+
+        it('sets a `hash` that matches the difficulty criteria',()=>{
+            expect(minedBlock.hash.substring(0, minedBlock.difficulty)).toEqual('0'.repeat(minedBlock.difficulty));
         });
     });
 
